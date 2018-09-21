7adebf6543b26c2f3e1770de2b600f23d85ab82e
Mohit Chawla <mohitchawla.iitdhn@gmail.com>
2018-09-21 16:44:34 -0400

    added gRPC related code

---
+++ pb/rearpb/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(name='rearpb',
+      version='0.1',
+      description='',
+      url='',
+      author='',
+      author_email='',
+      license='MIT',
+      packages=['rearpb'],
+      install_requires=[
+      ],
+      zip_safe=False)
