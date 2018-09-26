a1dfdc421d9e8aae342e436bd94aa931566683e7
Kriti Singh <kriti96.singh@gmail.com>
2018-09-26 16:10:51 -0400

    basic loop of training the model by forking on django server

--- board/views.py
+++ board/views.py
@@ -4,10 +4,13 @@ from __future__ import absolute_import
 from django.shortcuts import render
 from django.http import HttpResponse
 import sys
+import os
+from multiprocessing import Process
 
 import grpc
 from rearpb import  rearpb_pb2_grpc,rearpb_pb2
 
+from .model_manager import ModelManager
 from .common import exceptions
 
 # Create your views here.
@@ -31,5 +34,24 @@ def index(request):
 		return HttpResponse(response.message)
 	return HttpResponse("GRPC Server is unreachable")
 
-# def create(request):
+def create(request):
+	print ('Parent process pid:', os.getpid())
+	PATH_TO_DATASET = '/ctrsr_datasets/citeulike-a/users.dat'
+	model_controller_obj = ModelManager('BPR', 'train_samp', 'val_samp', 'test_samp', 'AUC', PATH_TO_DATASET)
+	
+	p = Process(target=ModelManager.sample_data_and_train, args=(model_controller_obj,))
+	p.start()
+	p.join(2)
+
+	try:
+		generated_model_id = model_controller_obj.model_id
+		err = "No err "
+		return HttpResponse(str(generated_model_id + err))
+	except:
+		print("Error in getting generated model-id")
+		p.terminate() # Force terminate training
+		return HttpResponse("Error in getting generated model-id")
+
+	
+
 	
