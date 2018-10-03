be881191027f18c4c64a2e99a604921a1555a852
Mohit Chawla <mohitchawla.iitdhn@gmail.com>
2018-10-03 14:54:50 -0400

    added file upload

--- board/views.py
+++ board/views.py
@@ -9,47 +9,57 @@ from multiprocessing import Process
 from .model_manager import ModelManager
 from .common import exceptions
 from django.views.generic.base import TemplateView
-
+import logging
 
 from .forms import UploadFileForm
 from .models import UploadFile
 
+logging.getLogger(__name__)
+
+
 def index(request):
-	return HttpResponse("All good, server is up")
+    return HttpResponse("All good, server is up")
+
+def save_uploaded_file(filename,file):
+    with open('data/'+filename, 'wb+') as destination:
+        #use chunks instead of read to avoid having large files in memory
+        for chunk in file.chunks():
+            destination.write(chunk)
+        logging.info("Uploaded file saved","file:",filename,"location:",destination)
 
 def upload(request):
-	if request.method == 'POST':
-		form = UploadFileForm(request.POST, request.FILES)
-		if form.is_valid():
-			for filename, file in request.FILES.items():
-				#TODO: send files to grpc server API here
-				print(filename,file.read())
-	else:
-		raise Exception("Invalid request")
-	return HttpResponse("Uploaded")
+    user_id = "1" #TODO: replace this with actual logged in user later
+    if request.method == 'POST':
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            for _, file in request.FILES.items():
+                save_uploaded_file(user_id+"_"+file.name, file)
+    else:
+        raise Exception("Invalid request")
+    return HttpResponse("Uploaded")
 
 
 class HomePage(TemplateView):
-	"""
-		This is a class based view for home page (/home)
-	"""
-	template_name = 'home.html'
+    """
+        This is a class based view for home page (/home)
+    """
+    template_name = 'home.html'
 
 
 def create(request):
-	print ('Parent process pid:', os.getpid())
-	PATH_TO_DATASET = '/ctrsr_datasets/citeulike-a/users.dat'
-	model_controller_obj = ModelManager('BPR', 'train_samp', 'val_samp', 'test_samp', 'AUC', PATH_TO_DATASET)
+    print ('Parent process pid:', os.getpid())
+    PATH_TO_DATASET = '/ctrsr_datasets/citeulike-a/users.dat'
+    model_controller_obj = ModelManager('BPR', 'train_samp', 'val_samp', 'test_samp', 'AUC', PATH_TO_DATASET)
 
-	p = Process(target=ModelManager.sample_data_and_train, args=(model_controller_obj,))
-	p.start()
-	p.join(2)
+    p = Process(target=ModelManager.sample_data_and_train, args=(model_controller_obj,))
+    p.start()
+    p.join(2)
 
-	try:
-		generated_model_id = model_controller_obj.model_id
-		err = "No err "
-		return HttpResponse(str(generated_model_id + err))
-	except:
-		print("Error in getting generated model-id")
-		p.terminate() # Force terminate training
-		return HttpResponse("Error in getting generated model-id")
+    try:
+        generated_model_id = model_controller_obj.model_id
+        err = "No err "
+        return HttpResponse(str(generated_model_id + err))
+    except:
+        print("Error in getting generated model-id")
+        p.terminate() # Force terminate training
+        return HttpResponse("Error in getting generated model-id")
--- recboard/settings.py
+++ recboard/settings.py
@@ -14,7 +14,6 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-print(BASE_DIR)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
@@ -124,4 +123,3 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'board/static'),
 )
-
